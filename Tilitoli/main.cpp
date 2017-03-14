e9c2827eef2bcf4282a53e98514e0bba503ade41
Czégényi Roland <czegenyi.roland@gmail.com>
2017-03-14 14:07:17 +0100

    Finished

--- Tilitoli/main.cpp
+++ Tilitoli/main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
+#include <conio.h>
+
+#define KEY_UP 119
+#define KEY_DOWN 115
+#define KEY_LEFT 97
+#define KEY_RIGHT 100
 
 class Tilitoli {
 protected:
     int m[3][3];
+    int x;
+    int y;
 public:
     void ertekadas(int m2[3][3]);
 
     int ellenorzes();
 
-    int le();
+    void kiir();
 
-    int fel();
+    void hely();
 
-    int jobb();
+    void le();
 
-    int bal();
+    void fel();
+
+    void jobb();
+
+    void bal();
 };
 
 void Tilitoli::ertekadas(int m2[3][3]) {
@@ -30,21 +42,105 @@ int Tilitoli::ellenorzes() {
         for (int j = 0; j < 3; j++)
             for (int s = 0; s < 3; s++)
                 for (int o = 0; o < 3; o++)
-                    if ((i != s || j != o) && (m[i][j] == m[s][o]))
+                    if ((i != s || j != o) && (m[i][j] == m[s][o])) {
+                        std::cout << "Hiba az ellenorzes soran";
                         return 0;
+                    }
     return 1;
 }
 
+void Tilitoli::kiir() {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++)
+            std::cout << m[i][j] << ' ';
+        std::cout << std::endl;
+    }
+}
+
+void Tilitoli::hely() {
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            if (m[i][j] == 0) {
+                x = i;
+                y = j;
+            }
+}
+
+void Tilitoli::le() {
+    hely();
+    if (x != 2) {
+        m[x][y] = m[x + 1][y];
+        m[x + 1][y] = 0;
+    }
+    ellenorzes();
+}
+
+void Tilitoli::fel() {
+    hely();
+    if (x != 0) {
+        m[x][y] = m[x - 1][y];
+        m[x - 1][y] = 0;
+    }
+    ellenorzes();
+}
+
+void Tilitoli::bal() {
+    hely();
+    if (y != 0) {
+        m[x][y] = m[x][y - 1];
+        m[x][y - 1] = 0;
+    }
+    ellenorzes();
+}
+
+void Tilitoli::jobb() {
+    hely();
+    if (y != 2) {
+        m[x][y] = m[x][y + 1];
+        m[x][y + 1] = 0;
+    }
+    ellenorzes();
+}
 
 int main() {
 
     Tilitoli tilitoli;
-    
+
     int m2[3][3] = {1, 2, 0,
                     4, 6, 3,
                     7, 5, 8};
 
     tilitoli.ertekadas(m2);
+
+    tilitoli.kiir();
+
+
+    int c = 0;
+    while (1) {
+        c = 0;
+
+        switch ((c = getch())) {
+            case KEY_UP:
+                tilitoli.fel();
+                tilitoli.kiir();
+                break;
+            case KEY_DOWN:
+                tilitoli.le();
+                tilitoli.kiir();
+                break;
+            case KEY_LEFT:
+                tilitoli.bal();
+                tilitoli.kiir();
+                break;
+            case KEY_RIGHT:
+                tilitoli.jobb();
+                tilitoli.kiir();
+                break;
+            default:
+                break;
+        }
+
+    }
 
     return 0;
 }
