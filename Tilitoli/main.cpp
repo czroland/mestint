b9f4786d4d641902e0e63da83f10468f5069e38d
Czégényi Roland <czegenyi.roland@gmail.com>
2017-03-14 13:00:15 +0100

    Adding some methods

--- Tilitoli/main.cpp
+++ Tilitoli/main.cpp
@@ -18,16 +18,33 @@ public:
 };
 
 void Tilitoli::ertekadas(int m2[3][3]) {
+
     for (int i = 0; i != 3; i++)
         for (int j = 0; j != 3; j++)
             m[i][j] = m2[i][j];
 }
 
 int Tilitoli::ellenorzes() {
+
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 3; j++)
+            for (int s = 0; s < 3; s++)
+                for (int o = 0; o < 3; o++)
+                    if ((i != s || j != o) && (m[i][j] == m[s][o]))
+                        return 0;
     return 1;
 }
 
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Tilitoli tilitoli;
+    
+    int m2[3][3] = {1, 2, 0,
+                    4, 6, 3,
+                    7, 5, 8};
+
+    tilitoli.ertekadas(m2);
+
     return 0;
 }
